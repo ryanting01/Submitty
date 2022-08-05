d4c2fc4a422536624922a7d66e3ca521b7841bc8
Shail Patel <shailpatel67@gmail.com>
2022-08-04 22:31:09 -0400

    initial work

--- site/ts/sw.js
+++ site/ts/sw.js
@@ -1,3 +1,16 @@
+// sw.js
+import { registerRoute, Route } from 'workbox-routing';
+import { CacheFirst } from 'workbox-strategies';
+
+// // A new route that matches same-origin image requests and handles
+// // them with the cache-first, falling back to network strategy:
+const imageRoute = new Route(({ request, sameOrigin }) => {
+  return sameOrigin && request.destination === 'image'
+}, new CacheFirst());
+
+// Register the new route
+registerRoute(imageRoute);
+
 const staticDevPort = 'Submittty';
 const assets = [
 ];
