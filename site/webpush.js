38f5773e8301e34141d17fafb17209401cfd01ac
immortalcodes <21112002mj@gmail.com>
2022-08-29 11:47:14 +0530

    data is fetched form postegres database and notifications sent

--- site/webpush.js
+++ site/webpush.js
@@ -1,23 +1,25 @@
- 
+const db_cred= require('../config/database.json'); 
 
+
+const { Client } = require("pg");
 const webpush = require('web-push');
 
-// VAPID keys should only be generated only once.
+// VAPID keys for validations and security
 const vapidKeys = {
   publicKey:
     'BBE6zGMtikC3Wdkma0TF4A6Qs-ehdhv_PpJX1hxkSUgVut6fijqO8OkbzQ5oXZ8zZBugvhj0ztlMgyYeWkmYBao',
   privateKey: 'yegbxXOSn3M5zF9UCvLAU8iOi-qh6iyFhZNet_G7Cx8'
 };
 
-
-
+// might want to have it stored in vagrant machine
 webpush.setVapidDetails(
   'mailto:example@yourdomain.org',
   vapidKeys.publicKey,
   vapidKeys.privateKey
 );
 
-// This is the same output of calling JSON.stringify on a PushSubscription
+
+// This is the hard coded push subsciption , this needs to be loaded from a database of stored pushURLS
 const pushSubscription = {endpoint:"https://fcm.googleapis.com/fcm/send/f9RRGp32wGQ:APA91bGoXkEHmBsIP2WsGVi-okE5GXu6iaNutyZ_Gr904dtq6FYzBirg1cjc7y7YGk3cdqelApUZbfPv6EJWp9paXQIJctixGyaFAt0DyuUc01ZhgOD7TyNdzZtucvTX4TqEdhtQFM34",
 expirationTime:null,
 keys:
@@ -26,12 +28,42 @@ auth:"Ba4JuXuoyAHSFHsbApQ92w"}}
 
 
 
+// Connecting the database
+const connectDB = async () => {
+  try {
+      const client = new Client({
+          user: db_cred.database_user,
+          host: db_cred.database_host,
+          database: "submitty_s22_sample",
+          password: db_cred.database_password,
+          port: db_cred.database_port
+      })
+
+      await client.connect();
+      const notifications = await client.query('SELECT * FROM notifications ');
+      await client.end();
+      return notifications;
+
+  } catch (error) {
+      console.log(error);
+  }
+}
 
 
+// sending the push notifications
 
-webpush.sendNotification(pushSubscription, JSON.stringify({
-    title: 'New Notification',
-    content: "This is a push notification!"
-  }));
+  const send_push_notifications = async () => {
+  const notifications_fetched = await connectDB();
+  notifications_fetched.rows.forEach(row => {
+    webpush.sendNotification(pushSubscription, JSON.stringify({
+      title: row.component,
+      content: row.content
+    }));
+    
+  });
+  console.log("notifications sent successfully")
+
+  }
 
 
+send_push_notifications();
