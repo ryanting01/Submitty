c268b715effc7fda6e6049582d5d6447f79f118a
immortalcodes <21112002mj@gmail.com>
2022-09-02 18:11:01 +0530

    push subs are fetched from database

---
+++ migration/migrator/migrations/master/20220902151412_push_subs.py
@@ -0,0 +1,33 @@
+"""Migration for the Submitty master database."""
+
+
+def up(config, database):
+    """
+    Run up migration.
+
+    :param config: Object holding configuration details about Submitty
+    :type config: migrator.config.Config
+    :param database: Object for interacting with given database for environment
+    :type database: migrator.db.Database
+    """
+    database.execute(
+    """
+    CREATE TABLE IF NOT EXISTS push_subsciptions (
+        key_column BIGSERIAL PRIMARY KEY,
+        user_id VARCHAR NOT NULL,
+        push_subscription TEXT NOT NULL
+    )
+    """
+    )
+
+
+def down(config, database):
+    """
+    Run down migration (rollback).
+
+    :param config: Object holding configuration details about Submitty
+    :type config: migrator.config.Config
+    :param database: Object for interacting with given database for environment
+    :type database: migrator.db.Database
+    """
+    pass
